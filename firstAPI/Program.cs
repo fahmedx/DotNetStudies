11a3b86e237bae0d53c379d01e1504003b7dbda0
fahmedx <fahmecanaldx@gmail.com>
2023-02-20 18:43:48 -0300

    Some  new updates on requests creation

--- firstAPI/Program.cs
+++ firstAPI/Program.cs
@@ -2,5 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello APIs!");
+app.MapGet("/user", () => new {name = "fsalim", age = 24});
+app.MapGet("/AddHeader",(HttpResponse response)=> {
+    response.Headers.Add("Test","fsalim");
+    return "Success";
+    });
 
 app.Run();
